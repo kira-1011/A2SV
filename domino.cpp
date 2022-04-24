135b8396699697129579dc1eb9bab9479b8ad737
kira-1011 <kirakstade@gmail.com>
2022-04-24 12:50:31 +0300

    CodeForces: A. Domino piling

---
+++ domino.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int m = 0, n = 0, max = 0;
+
+    cin >> m >> n;
+
+    max = (m * n) / 2;
+
+    cout << max << endl;
+
+    return 0;
+}

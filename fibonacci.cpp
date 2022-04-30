1efe7b8acd85e325b075699743e9eb1d0b95db6c
kira-1011 <kirakstade@gmail.com>
2022-04-30 21:57:13 +0300

    LeetCode: Fibonacci Number

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int fib(int n) {
+        const double golden_ratio = 1.6180339887498948482;
+        
+        int res = (pow(golden_ratio, n) - pow((1 - golden_ratio), n)) / sqrt(5);
+        
+        return res;
+    }
+    
+};
+
+
+
+
+
+class Solution {
+public:
+    int fib(int n) {
         
         if(n == 1)
             return 1;

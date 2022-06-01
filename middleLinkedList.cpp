6d28dca7655413a40f62b4779cfe3e6d24b608c6
kira-1011 <kirakstade@gmail.com>
2022-06-01 18:47:50 +0300

    LeetCode: middle of a linked list

--- middleLinkedList.cpp
+++ middleLinkedList.cpp
@@ -8,27 +8,30 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    ListNode* middleNode(ListNode* head) {
+    ListNode *middleNode(ListNode *head)
+    {
         int middle = 1;
         ListNode *temp = head;
-        
-        
-        //calculate the total nodes
-        while(temp -> next != NULL){
-            middle ++;
-            temp = temp -> next;
+
+        // calculate the total nodes
+        while (temp->next != NULL)
+        {
+            middle++;
+            temp = temp->next;
         }
-        
+
         middle = (middle / 2) + 1;
         temp = head;
-        
-        //search for the middle node
-        for(int i = 1; i < middle; i++){
-            temp = temp -> next;
+
+        // search for the middle node
+        for (int i = 1; i < middle; i++)
+        {
+            temp = temp->next;
         }
-        
+
         return temp;
     }
 };

a2dffda71cd2e1d8e4b6d16a794b517b50f857b2
kira-1011 <kirakstade@gmail.com>
2022-07-12 13:21:09 -0900

    geeks for geeks: Heap sort

---
+++ heapSort.cpp
@@ -0,0 +1,100 @@
+// { Driver Code Starts
+// C++ program for implementation of Heap Sort
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// The functions should be written in a way that array become sorted 
+// in increasing order when heapSort() is called.
+
+class Solution
+{
+    public:
+    //Heapify function to maintain heap property.
+    void heapify(int arr[], int n, int i)  
+    {
+      // Your Code Here
+        if(i >= n / 2)
+            return;
+          
+        int leftChild = 2 * i + 1;
+        int rightChild = 2 * i + 2;
+        int largest = i;
+        
+        if(leftChild < n && arr[leftChild] > arr[largest])
+            largest = leftChild;
+        
+        if(rightChild < n && arr[rightChild] > arr[largest])
+            largest = rightChild;
+            
+        if(largest != i)
+        {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            heapify(arr, n, largest);
+        } 
+          
+    }
+
+    public:
+    //Function to build a Heap from array.
+    void buildHeap(int arr[], int n)  
+    { 
+        // Your Code Here
+        for(int i = (n / 2) - 1; i >= 0; i--)
+            heapify(arr, n, i);
+    }
+
+    
+    public:
+    //Function to sort an array using Heap Sort.
+    void heapSort(int arr[], int n)
+    {
+        //code here
+        buildHeap(arr, n);
+        
+        for(int i = n - 1; i > 0; i--)
+        {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            
+            n--;
+            
+            heapify(arr, n, 0);
+        }
+    }
+};
+
+
+
+
+// { Driver Code Starts.
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Driver program to test above functions
+int main()
+{
+    int arr[1000000],n,T,i;
+    scanf("%d",&T);
+    while(T--){
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+    Solution ob;
+    ob.heapSort(arr, n);
+    printArray(arr, n);
+    }
+    return 0;
+}
+  // } Driver Code Ends

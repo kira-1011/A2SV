2b971a66e06e5ce2ee054ff5094c4f981be6c3b4
kira-1011 <kirakstade@gmail.com>
2022-07-18 03:15:01 -0900

    LeetCode: Next Greater Element I

--- nextGreater.cpp
+++ nextGreater.cpp
@@ -1,40 +1,72 @@
 class Solution {
-private:
-    int indexOf(vector<int> nums, int num){
-        auto it = find(nums.begin(), nums.end(), num);
-        
-        if (it != nums.end()) 
-        {
-            int index = it - nums.begin();
-            return index;
-        }
-        
-        return -1;
-        
-    }
-    
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> nextGreaterElements;
-        bool found = false;
         
-        for(int i = 0, n = nums1.size(), index; i < n; i++){
-            index = indexOf(nums2,nums1[i]);
-            found = false;
-            for(int j = index + 1, n = nums2.size(); j < n; j++ ){
-                 if(nums2[index] < nums2[j]){
-                     nextGreaterElements.push_back(nums2[j]);
-                     found = true;
-                     break;
-                 }
+        unordered_map<int, int> hashMap;
+        unordered_map<int, int> :: iterator itr;
+        vector<int> monotonicStack;
+        vector<int> answer;
+        
+        for(int i = 0, n = nums2.size(); i < n;  i++)
+            hashMap.insert(make_pair(nums2[i], i));
+        
+        int i = 0;
+        
+        itr = hashMap.find(nums1[i]);
+        
+        int j = itr -> second;
+        
+        int n = nums1.size();
+        
+        int n2 = nums2.size();
+        
+        monotonicStack.push_back(nums2[j]);
+        
+        j++;
+        
+        while(i < n)
+        {
+            if(j >= n2)
+            {
+                answer.push_back(-1);
+                i++;
+                
+                if(i >= n)
+                    break;
+                
+                itr = hashMap.find(nums1[i]);
+                j = itr -> second;
+                monotonicStack.push_back(nums2[j]);
+                j++;
             }
             
-            if(!found)
-                nextGreaterElements.push_back(-1);
-            
+            else if(nums2[j] > monotonicStack.back())
+            {
+                int popped = monotonicStack.back();
+                
+                monotonicStack.pop_back();
+                
+                if(popped == nums1[i])
+                {
+                    answer.push_back(nums2[j]);
+                    i++;
+                    
+                    if(i >= n)
+                        break;
+                    
+                    itr = hashMap.find(nums1[i]);
+                    j = itr -> second;
+                    monotonicStack.push_back(nums2[j]);
+                    j++;
+                }
+            }
+            else
+            {
+                monotonicStack.push_back(nums2[j]);
+                j++;
+            }
         }
         
-        return nextGreaterElements;
-        
+        return answer;
     }
 };

242cbbe192714cb1b4124fd521284fe7594f4f70
kira-1011 <kirakstade@gmail.com>
2022-12-23 00:17:27 +0300

    LeetCode: Find players with Zero or one losses

---
+++ playersWithZeroOneOrZeroLoses.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+
+        # Initialize dictionary that contains players lose count
+        lose_count = {}
+        answer = [[],[]]
+
+        # Count the number of times a player lost
+        for players in matches:
+            loser = players[1]
+            winner = players[0]
+
+            count = lose_count.get(loser, 0) + 1
+            lose_count[loser] = count
+
+            count = lose_count.get(winner, 0)
+            lose_count[winner] = count
+        
+        # Check players that have not lost any matches or lost exactly one match
+        for player in lose_count:
+            count = lose_count[player]
+            if count == 0:
+                answer[0].append(player)
+            elif count == 1:
+                answer[1].append(player)
+        
+        # Sort answer
+        answer[0].sort()
+        answer[1].sort()
+
+        return answer

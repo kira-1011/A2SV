420bfc78908cf9a53c5562359397ebd234fadc48
kira-1011 <86254335+kira-1011@users.noreply.github.com>
2023-03-01 11:11:25 +0300

    Update permutationString.py

--- progress_sheet/permutationString.py
+++ progress_sheet/permutationString.py
@@ -4,28 +4,30 @@ class Solution:
         len1 = len(s1)
         len2 = len(s2)
         s1_count = Counter(s1)
-        s2_count = defaultdict(int)
 
         if len1 > len2:
             return False
 
         # Construct window
         for i in range(len1):
-            s2_count[s2[i]] += 1
+            if s2[i] in s1_count:
+                 s1_count[s2[i]] -= 1
         
         # compare the letter count of s2 window with s1
         for right in range(len1, len2):
-
-            if s1_count == s2_count:
-                return True
             
-            s2_count[s2[right]] += 1
-            s2_count[s2[right - len1]] -= 1
+            # Check if all values are 0 in dictionary
+            if not any(s1_count.values()):
+                return True
 
-            if s2_count[s2[right - len1]] == 0:
-                del s2_count[s2[right - len1]]
-        
-        if s1_count == s2_count:
+            if s2[right] in s1_count:
+                s1_count[s2[right]] -= 1
+            
+            if s2[right - len1] in s1_count:
+                s1_count[s2[right - len1]] += 1
+            
+        # Check if all values are 0 in dictionary
+        if not any(s1_count.values()):
             return True
 
         return False

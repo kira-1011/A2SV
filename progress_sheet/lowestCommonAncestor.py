3e884fb95c9f21974f3b05253ce9cbeb46999d42
kira-1011 <86254335+kira-1011@users.noreply.github.com>
2023-03-08 15:41:18 +0300

    Leetcode : Lowest common ancestor node

---
+++ progress_sheet/lowestCommonAncestor.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        # traverse
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q) 
+
+        # if they're in different subtrees or one of the target nodes are found return the current node which is the LCA 
+        return root

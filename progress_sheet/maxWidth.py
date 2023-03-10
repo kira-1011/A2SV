1c6051357e3ebabd4409a3ed2eac5d3164dbb789
kira-1011 <86254335+kira-1011@users.noreply.github.com>
2023-03-10 18:07:44 +0300

    Leetcode : Maximum Width of Binary Tree

--- progress_sheet/maxWidth.py
+++ progress_sheet/maxWidth.py
@@ -6,44 +6,32 @@
 #         self.right = right
 class Solution:
 
-    def BFS(self, root, queue, nodes):
-
-        if not root:
-            return
-        
-        if not queue:
-            return
-        
-        length = len(queue)
-        level = []
-
-        for i in range(length):
-            
-            curr_root, col = queue.popleft()
-
-            if curr_root:
-                level.append(col)
-                queue.append([curr_root.left, 2 * col])
-                queue.append([curr_root.right, (2 * col) + 1])
-        
-        if level:
-            nodes.append(level)
-        
-        self.BFS(root.left, queue, nodes)
-        self.BFS(root.right, queue, nodes)
-
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         
         level_order = []
         queue = deque()
         queue.append([root, 0])
-
-        self.BFS(root, queue, level_order)
-        
         max_width = 0
 
-        for level in level_order:
-            max_range = level[-1] - level[0] + 1
-            max_width = max(max_width, max_range)
+        # Level order traversal
+        while queue:
+
+            length = len(queue)
+            level = []
+
+            for i in range(length):
+                
+                curr_root, col = queue.popleft()
+
+                if curr_root:
+                    level.append(col)
+                    queue.append([curr_root.left, 2 * col])
+                    queue.append([curr_root.right, (2 * col) + 1])
+            
+            if level:
+                # Find max width
+                max_range = level[-1] - level[0] + 1
+                max_width = max(max_width, max_range)
+                level_order.append(level)
 
         return max_width

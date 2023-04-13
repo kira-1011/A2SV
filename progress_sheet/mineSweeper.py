743e6db34acb651a3aff68c5ea416da6183ea428
kira-1011 <86254335+kira-1011@users.noreply.github.com>
2023-04-12 21:17:13 -0900

    Update mineSweeper.py

--- progress_sheet/mineSweeper.py
+++ progress_sheet/mineSweeper.py
@@ -2,23 +2,31 @@ class Solution:
 
     def __init__(self):
         self.directions = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, -1), (-1, 1), (1, -1)]
+    
+    def inBound(self, board, r, c):
+        return -1 < r < len(board) and -1 < c < len(board[0])
 
-
-    def dfsGrid(self, board, row, col, visited):
-        
-        visited.add((row, col))
-        inBound = lambda r, c: -1 < r < len(board) and -1 < c < len(board[0])
+    def countMines(self, board, row, col):
         
         adjacent = 0
 
         # count adjacent mines
-        for direction in self.directions:
+        for row_change, col_change in self.directions:
 
-            new_row = row + direction[0]
-            new_col = col + direction[1]
+            new_row = row + row_change
+            new_col = col + col_change
 
-            if inBound(new_row, new_col) and board[new_row][new_col] == 'M':
+            if self.inBound(board, new_row, new_col) and board[new_row][new_col] == 'M':
                 adjacent += 1
+
+        return adjacent
+
+    def dfsGrid(self, board, row, col, visited):
+        
+        visited.add((row, col))
+
+        # count adjacent mines
+        adjacent = self.countMines(board, row, col)
 
         if adjacent:
             board[row][col] = str(adjacent)
@@ -27,11 +35,11 @@ class Solution:
         else:
             board[row][col] = 'B'
 
-        for direction in self.directions:
-            new_row = row + direction[0]
-            new_col = col + direction[1]
+        for row_change, col_change in self.directions:
+            new_row = row + row_change
+            new_col = col + col_change
 
-            if inBound(new_row, new_col) and (new_row, new_col) not in visited:
+            if self.inBound(board, new_row, new_col) and (new_row, new_col) not in visited:
                 self.dfsGrid(board, new_row, new_col, visited)
 
     def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
@@ -43,4 +51,3 @@ class Solution:
         self.dfsGrid(board, click[0], click[1], set())
 
         return board
-        
